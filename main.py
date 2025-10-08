dd5cf6063667404d9992357099b3d0f4b99306a6
Erhval <valentin.erhard@gmail.com>
2025-10-08 21:53:35 +0200

    Threading, basic computeur vision, and possibility to execute other code

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from PIL import Image, ImageGrab
 import numpy as np
 import cv2 as cv
 import time
+from threading import Thread
+
+class MainAgent:
+    def __init__(self) -> None:
+        self.screenshot = None
+
+        print(" MainAgent setup complete...")
 
 def update_screen():
     t0 = time.time()
@@ -10,14 +17,22 @@ def update_screen():
         screenshot = ImageGrab.grab()
         screenshot = np.array(screenshot)
         screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
-
-        cv.imshow("Comupteur vision", screenshot)
+        cv.imshow("Computeur vision", screenshot)
         key = cv.waitKey(1)
         if key == ord('q'):
             break
         ex_time = time.time() - t0
-        print("FPS: " + str(1/(ex_time)))
+        # print("FPS: " + str(1/(ex_time)))
         t0 = time.time()
 
 if __name__ == "__main__":
-    update_screen()
+    main_agent = MainAgent()
+
+    update_screen_thread = Thread(
+        target=update_screen,
+        args=(),
+        name="update screen thread",
+        daemon=False)
+    #QU'EST CE QUE QU'UN DAEMON ?!? : C'est un process qui tourne "behind the scene" On le met en false pour que le programme complet soit visible. D'autre thread passerons en true 
+    update_screen_thread.start()
+    print("Thread started")
